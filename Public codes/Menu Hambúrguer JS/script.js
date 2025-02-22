50b8e44ecabce1c5ab3a52e04f6371127dab1162
Eric Gonçalves <166281170+EricRochaGoncalves@users.noreply.github.com>
2025-02-22 13:07:34 -0300

    Atualizar o script.js

--- Public codes/Menu Hambúrguer JS/script.js
+++ Public codes/Menu Hambúrguer JS/script.js
@@ -1,9 +1,20 @@
 const btnMenu = document.getElementById('btn-menu');
 const nav = document.getElementById('nav');
 
-// Função para alternar a visibilidade do menu
+// Função para alternar a visibilidade do menu e alterar o ícone
 function toggleMenu() {
     nav.classList.toggle('active'); // Adiciona/remove a classe 'active'
+
+    const icon = btnMenu.querySelector('i'); // Seleciona o ícone dentro do botão
+    if (nav.classList.contains('active')) {
+        // Se o nav tiver a classe 'active', muda para o ícone de 'fechar'
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-times');
+    } else {
+        // Caso contrário, volta para o ícone de 'menu'
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+    }
 }
 
 // Evento de clique no botão do menu
